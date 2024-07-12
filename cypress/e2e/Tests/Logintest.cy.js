2781bbdc9fe916444948f1354c5453afdf736283
cypress1980 <76464201+cypress1980@users.noreply.github.com>
2024-07-12 10:32:56 +0530

    Update Logintest.cy.js

--- cypress/e2e/Tests/Logintest.cy.js
+++ cypress/e2e/Tests/Logintest.cy.js
@@ -1,50 +1,41 @@
 // <reference types="cypress" />
 
 import LoginPage from "../Page/loginPage.cy.js";
-
 describe('Login Test', () => {
     let testdata;
+    const loginpage = new LoginPage();
 
-     const loginpage = new LoginPage();
-
-beforeEach(() => {
-    cy.fixture('credential').then( (testdata_original)=> {
-        testdata = testdata_original;
-      });
-       // cy.visit('/');  
-       cy.visit ('https://ecommerce-playground.lambdatest.io/index.php?route=account/login');
+    beforeEach(() => {
+    cy.fixture('credential.json').then( (testdata_original)=> {testdata = testdata_original});
+    cy.visit('https://ecommerce-playground.lambdatest.io/index.php?route=account/login');
     });
 
-    it.skip('Login Test', () => {
-        loginpage.loginbuttonexist().contains('Login').should('be.visible');
-   
+    
+    it('Login Test', () => {
+        loginpage.loginbuttonexist()
     })
-
 
     it('valid credential', () => {
         loginpage.enteremailAddress(testdata.emailAddress);
         loginpage.enterPassword(testdata.password);
         loginpage.clickLogin();
         cy.url().should('include', 'https://ecommerce-playground.lambdatest.io/index.php?route=account/account');
-        loginpage.usr_logged_in();
-        cy.wait(1000);
-        //loginpage.user_logout();
-
+        loginpage.user_logout()
     })
 
     it('Enter wrong email and password', () => {
-        loginpage.enterUsername(testdata.invalid_username);
+        loginpage.enteremailAddress(testdata.invalid_username);
         loginpage.enterPassword(testdata.invalid_password);
         loginpage.clickLogin();
         cy.url().should('not.include' , '/web/index.php/dashboard/index');
-        loginpage.getErrorMessage("Invalid credentials");
-       })
+        loginpage.getErrorMessage();
+    })
 
-       it('Enter empty email and password', () => {
+    it('Enter empty email and password', () => {
         loginpage.clickLogin();
-        cy.contains('Required').should('be.visible');
-        cy.url().should('not.include' , '/web/index.php/dashboard/index');
-       })
+        loginpage.getErrorMessage();
+        //cy.url().should('not.include' , '/web/index.php/dashboard/index');
+    })
 
 
 });
